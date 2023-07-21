88dfa7271a8ed993d531fb84e158d935e2385ab2
Agure-Ogonji <56344381+Agure-Ogonji@users.noreply.github.com>
2023-07-21 06:11:23 +0000

    Dynamic data and API route

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id' : 1,
+    'title' : 'Data Analyst',
+    'location' : 'Kisumu, Kenya',
+    'salary' : 'KES. 1,000,000'
+  },
+  {
+    'id' : 2,
+    'title' : 'Data Scientist',
+    'location' : 'Paris, France',
+    'salary' : 'KES. 2,000,000'
+  },
+  {
+    'id' : 3,
+    'title' : 'Videographer',
+    'location' : 'Geneve, Switzerland',
+    'salary' : 'USD. 5000'
+  },
+  {
+    'id' : 4,
+    'title' : 'Software Developer',
+    'location' : 'Remote',
+    'salary' : 'USD. 10,000'
+  }
+]
 
 @app.route("/")
 
 def My_family():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='ChakTek')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
